197be3df9eb13723c13108e51b5b7c2de08765bb
airokay <airokairok@gmail.com>
2014-09-20 16:59:27 -0700

    Project Commit
    start

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/RankingsActivity.java
@@ -0,0 +1,18 @@
+package com.garpr.android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.garpr.android.R;
+
+public class RankingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rankings);
+
+    }
+
+
+}

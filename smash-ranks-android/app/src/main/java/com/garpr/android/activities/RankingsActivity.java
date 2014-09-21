d01e41423c011bbed4214b1669740cb4d3e1fe0e
airokay <airokairok@gmail.com>
2014-09-20 19:16:38 -0700

    Network with Json on Ranking activity
    omg

--- smash-ranks-android/app/src/main/java/com/garpr/android/activities/RankingsActivity.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/RankingsActivity.java
@@ -1,7 +1,14 @@
 package com.garpr.android.activities;
 
 
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.VolleyError;
 import com.garpr.android.R;
+import com.garpr.android.misc.Networking;
+
+import org.json.JSONObject;
 
 
 public class RankingsActivity extends BaseActivity {
@@ -12,5 +19,25 @@ public class RankingsActivity extends BaseActivity {
         return R.layout.activity_rankings;
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        downloadRankings();
+    }
 
+    private void downloadRankings(){
+        Networking.Callback callback = new Networking.Callback() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RankingsActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(RankingsActivity.this, response.toString(), Toast.LENGTH_LONG).show();
+            }
+        };
+        Networking.getRankings(callback);
+
+    }
 }

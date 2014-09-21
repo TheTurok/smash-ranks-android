e061301458daea03a260eb4eefd4a7f4705cad25
ScootrNova <scootrnova@gmail.com>
2014-09-20 18:03:56 -0700

    created PlayersActivity

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/PlayersActivity.java
@@ -0,0 +1,16 @@
+package com.garpr.android.activities;
+
+
+import com.garpr.android.R;
+
+
+public class PlayersActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_players;
+    }
+
+
+}

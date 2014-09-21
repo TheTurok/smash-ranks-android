71804b10272186b4ed8b6570a6826bebb4a85a7b
airokay <airokairok@gmail.com>
2014-09-20 18:24:50 -0700

    created player activity

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.garpr.android.activities;
+
+
+import com.garpr.android.R;
+
+public class PlayerActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_player ;
+    }
+}

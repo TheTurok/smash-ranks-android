cabf4421129e7391b3d23e2e54f371062ce7c1b0
ScootrNova <scootrnova@gmail.com>
2014-09-20 17:53:24 -0700

    RankingsActivity now extends from BaseActivity

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/RankingsActivity.java
@@ -0,0 +1,16 @@
+package com.garpr.android.activities;
+
+
+import com.garpr.android.R;
+
+
+public class RankingsActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_rankings;
+    }
+
+
+}

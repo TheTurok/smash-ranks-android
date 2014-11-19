64ee529d6976704a8e7920c5fa6a25602ee3f426
Charles Madere <scootrnova@gmail.com>
2014-11-18 22:02:28 -0800

    created BaseFragmentActivity class for easy Fragment-holding Activities

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/BaseFragmentActivity.java
@@ -0,0 +1,61 @@
+package com.garpr.android.activities;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.garpr.android.R;
+
+
+abstract class BaseFragmentActivity extends BaseActivity {
+
+
+    private Fragment mFragment;
+
+
+
+
+    protected abstract Fragment createFragment();
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_base_fragment;
+    }
+
+
+    protected int getFragmentViewId() {
+        return R.id.activity_base_fragment_content;
+    }
+
+
+    protected Fragment getFragment() {
+        return mFragment;
+    }
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        prepareFragment();
+    }
+
+
+    protected void prepareFragment() {
+        final FragmentManager fManager = getSupportFragmentManager();
+        final int fragmentViewId = getFragmentViewId();
+        mFragment = fManager.findFragmentById(fragmentViewId);
+
+        if (mFragment == null) {
+            mFragment = createFragment();
+
+            final FragmentTransaction fTransaction = fManager.beginTransaction();
+            fTransaction.add(fragmentViewId, mFragment);
+            fTransaction.commit();
+        }
+    }
+
+
+}

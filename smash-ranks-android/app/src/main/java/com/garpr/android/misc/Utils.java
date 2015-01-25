f7a5515ade5f2ba593d8da26fbadaa14a7e3709e
Charles Madere <scootrnova@gmail.com>
2015-01-25 12:58:05 -0800

    removed unused Utils method

--- smash-ranks-android/app/src/main/java/com/garpr/android/misc/Utils.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/misc/Utils.java
@@ -37,26 +37,6 @@ public final class Utils {
     }
 
 
-    /**
-     * Allows you to safely and easily close a bunch of stream-type objects. Note that SQL objects
-     * absolutely can't be closed using this method (this is a bug with Android in versions before
-     * API 16).
-     */
-    public static void closeCloseables(final Closeable... closeables) {
-        if (closeables != null && closeables.length >= 1) {
-            for (final Closeable closeable : closeables) {
-                if (closeable != null) {
-                    try {
-                        closeable.close();
-                    } catch (final IOException e) {
-                        // this Exception can be safely ignored
-                    }
-                }
-            }
-        }
-    }
-
-
     public static void hideMenuItems(final MenuItem... items) {
         setMenuItemsVisibility(false, items);
     }

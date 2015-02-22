b0f01f974a36b272f007af701c2f0075459f11dd
Charles Madere <scootrnova@gmail.com>
2015-02-22 00:28:33 -0800

    database now enforces foreign key constraints

--- smash-ranks-android/app/src/main/java/com/garpr/android/data2/Database.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/Database.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 
 
 public final class Database extends SQLiteOpenHelper {
@@ -13,6 +14,16 @@ public final class Database extends SQLiteOpenHelper {
     public Database(final Context context, final String name, final CursorFactory factory,
             final int version) {
         super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onConfigure(final SQLiteDatabase db) {
+        super.onConfigure(db);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            db.setForeignKeyConstraintsEnabled(true);
+        }
     }
 
 

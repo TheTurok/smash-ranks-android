79e3515a374682799538344ecb4e67b1723f43bc
Charles Madere <scootrnova@gmail.com>
2015-02-22 00:41:10 -0800

    added new Response and ResponseOnUi classes

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/Response.java
@@ -0,0 +1,80 @@
+package com.garpr.android.data2;
+
+
+import com.garpr.android.misc.Heartbeat;
+import com.garpr.android.misc.Utils;
+
+import java.lang.ref.WeakReference;
+
+
+/**
+ * The result of an API call will be delivered here. Activities / Fragments / whatever that want
+ * to receive the result of an API call must instantiate one of these (or one of its child classes).
+ */
+public abstract class Response<T> implements Heartbeat {
+
+
+    private final String mTag;
+    protected final WeakReference<Heartbeat> mHeartbeat;
+
+
+
+
+    /**
+     * constructor
+     *
+     * @param tag
+     * The name of your Activity / Fragment / whatever. This should probably have been created
+     * using "Blah.class.getSimpleName()".
+     *
+     * @param heartbeat
+     * A {@link Heartbeat} so that we can tell if the class that wants to receive the result
+     * of the API response is still alive.
+     *
+     * @throws IllegalArgumentException
+     * If tag is an invalid String or heartbeat is null, then this Exception will be thrown.
+     */
+    public Response(final String tag, final Heartbeat heartbeat) throws IllegalArgumentException {
+        if (!Utils.validStrings(tag) || heartbeat == null) {
+            throw new IllegalArgumentException("tag (" + tag + ") and Heartbeat (" +
+                    heartbeat + ") cannot be null!");
+        }
+
+        mTag = tag;
+        mHeartbeat = new WeakReference<>(heartbeat);
+    }
+
+
+    public abstract void error(final Exception e);
+
+
+    public final String getResponderName() {
+        return mTag;
+    }
+
+
+    /**
+     * @return
+     * Returns true if the class that wants to receive the result of the API response is still
+     * alive
+     */
+    @Override
+    public final boolean isAlive() {
+        final Heartbeat heartbeat = mHeartbeat.get();
+        return heartbeat != null && heartbeat.isAlive();
+    }
+
+
+    /**
+     * Success! The API call has completed without any issue
+     */
+    public abstract void success(final T object);
+
+
+    @Override
+    public final String toString() {
+        return getResponderName();
+    }
+
+
+}
---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/ResponseOnUi.java
@@ -0,0 +1,82 @@
+package com.garpr.android.data2;
+
+
+import com.garpr.android.misc.HeartbeatWithUi;
+
+
+/**
+ * The result of an API call that must respond on the UI thread will be delivered here
+ */
+public abstract class ResponseOnUi<T> extends Response<T> implements HeartbeatWithUi {
+
+
+    /**
+     * see the super constructor
+     */
+    public ResponseOnUi(final String tag, final HeartbeatWithUi heartbeat) throws
+            IllegalArgumentException {
+        super(tag, heartbeat);
+    }
+
+
+    @Override
+    public final void error(final Exception e) {
+        if (isAlive()) {
+            final Runnable action = new Runnable() {
+                @Override
+                public void run() {
+                    if (isAlive()) {
+                        errorOnUi(e);
+                    }
+                }
+            };
+
+            runOnUi(action);
+        }
+    }
+
+
+    /**
+     * a major error has occurred
+     */
+    public abstract void errorOnUi(final Exception e);
+
+
+    /**
+     * Runs the given {@link Runnable} on the UI thread if the {@link HeartbeatWithUi} is still
+     * alive
+     */
+    @Override
+    public final void runOnUi(final Runnable action) {
+        final HeartbeatWithUi heartbeat = (HeartbeatWithUi) mHeartbeat.get();
+
+        if (heartbeat != null && heartbeat.isAlive()) {
+            heartbeat.runOnUi(action);
+        }
+    }
+
+
+    @Override
+    public final void success(final T object) {
+        if (isAlive()) {
+            final Runnable action = new Runnable() {
+                @Override
+                public void run() {
+                    if (isAlive()) {
+                        successOnUi(object);
+                    }
+                }
+            };
+
+            runOnUi(action);
+        }
+    }
+
+
+    /**
+     * Success! The API call has completed without any issue
+     */
+    public abstract void successOnUi(final T object);
+
+
+}

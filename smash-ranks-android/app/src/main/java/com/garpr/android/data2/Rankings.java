495dd4798b8956a8004a3a9356408890429c39da
Charles Madere <scootrnova@gmail.com>
2015-02-22 23:19:22 -0800

    put in place boilerplate for retrieving rankings

--- smash-ranks-android/app/src/main/java/com/garpr/android/data2/Rankings.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/Rankings.java
@@ -3,7 +3,14 @@ package com.garpr.android.data2;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.garpr.android.misc.Constants;
+import com.garpr.android.models.Player;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public final class Rankings {
@@ -24,6 +31,62 @@ public final class Rankings {
                 "FOREIGN KEY (" + Constants.REGION_ID + ") REFERENCES " + Regions.TAG + '(' + Constants.ID + "));";
 
         db.execSQL(sql);
+    }
+
+
+    public static void get(final Response<ArrayList<Player>> response, final boolean clear) {
+        new RankingsCall(response, clear).start();
+    }
+
+
+    public static void get(final Response<ArrayList<Player>> response, final String regionId,
+            final boolean clear) {
+        new RankingsCall(response, regionId, clear).start();
+    }
+
+
+
+
+    private static final class RankingsCall extends RegionBasedCall<ArrayList<Player>> {
+
+
+        private static final String TAG = "RankingsCall";
+
+        private final boolean mClear;
+
+
+        private RankingsCall(final Response<ArrayList<Player>> response, final boolean clear)
+                throws IllegalArgumentException {
+            super(response);
+            mClear = clear;
+        }
+
+
+        private RankingsCall(final Response<ArrayList<Player>> response, final String regionId,
+                final boolean clear) throws IllegalArgumentException {
+            super(response);
+            mClear = clear;
+        }
+
+
+        @Override
+        String getCallName() {
+            return TAG;
+        }
+
+
+        @Override
+        JsonObjectRequest getRequest() {
+            return null;
+        }
+
+
+        @Override
+        void onJSONResponse(final JSONObject json) throws JSONException {
+
+        }
+
+
     }
 
 

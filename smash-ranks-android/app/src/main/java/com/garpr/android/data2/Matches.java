0cfed8c8ab8d968b78ee96fd1470d9a201d870fa
Charles Madere <scootrnova@gmail.com>
2015-02-22 16:11:49 -0800

    tiny cleanup in Matches DB schema

--- smash-ranks-android/app/src/main/java/com/garpr/android/data2/Matches.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/Matches.java
@@ -16,10 +16,10 @@ public final class Matches {
 
     static void createTable(final SQLiteDatabase db) {
         final String sql = "CREATE TABLE IF NOT EXISTS " + TAG + " (" +
-                Constants.RESULT + " TEXT NOT NULL, " +
                 Constants.PLAYER_1_ID + " TEXT NOT NULL , " +
                 Constants.PLAYER_2_ID + " TEXT NOT NULL, " +
                 Constants.REGION_ID + " TEXT NOT NULL, " +
+                Constants.RESULT + " TEXT NOT NULL, " +
                 Constants.TOURNAMENT_ID + " TEXT NOT NULL, " +
                 "FOREIGN KEY (" + Constants.PLAYER_1_ID + ") REFERENCES " + Players.TAG + '(' + Constants.ID + "), " +
                 "FOREIGN KEY (" + Constants.PLAYER_2_ID + ") REFERENCES " + Players.TAG + '(' + Constants.ID + "), " +

800ede6a28ae7394a6e5ab9e6062847303c2b01d
Charles Madere <scootrnova@gmail.com>
2015-02-25 22:21:02 -0800

    filled out some of the Regions class

--- smash-ranks-android/app/src/main/java/com/garpr/android/data2/Regions.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data2/Regions.java
@@ -1,9 +1,19 @@
 package com.garpr.android.data2;
 
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.garpr.android.misc.Constants;
+import com.garpr.android.models2.Region;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public final class Regions {
@@ -21,6 +31,122 @@ public final class Regions {
                 "PRIMARY KEY (" + Constants.ID + "));";
 
         db.execSQL(sql);
+    }
+
+
+    public static void get(final Response<ArrayList<Region>> response, final boolean clear) {
+        new RegionsCall(response, clear).start();
+    }
+
+
+
+
+    private static final class RegionsCall extends Call<ArrayList<Region>> {
+
+
+        private static final String TAG = "RegionsCall";
+
+        private final boolean mClear;
+
+
+        private RegionsCall(final Response<ArrayList<Region>> response, final boolean clear)
+                throws IllegalArgumentException {
+            super(response);
+            mClear = clear;
+        }
+
+
+        private void clearThenMake() {
+            final SQLiteDatabase database = Database.start();
+            database.delete(Players.TAG, null, null);
+            Database.stop();
+
+            super.make();
+        }
+
+
+        @Override
+        String getCallName() {
+            return TAG;
+        }
+
+
+        @Override
+        JsonObjectRequest getRequest() {
+            return null;
+        }
+
+
+        @Override
+        void make() {
+            if (mClear) {
+                clearThenMake();
+            } else {
+                readThenMake();
+            }
+        }
+
+
+        @Override
+        void onJSONResponse(final JSONObject json) throws JSONException {
+            final JSONArray regionsJSON = json.getJSONArray(Constants.REGIONS);
+            final int regionsLength = regionsJSON.length();
+            final ArrayList<Region> regions = new ArrayList<>(regionsLength);
+
+            for (int i = 0; i < regionsLength; ++i) {
+                final JSONObject regionJSON = regionsJSON.getJSONObject(i);
+                final Region region = new Region(regionJSON);
+                regions.add(region);
+            }
+
+            final SQLiteDatabase database = Database.start();
+            database.beginTransaction();
+
+            for (final Region region : regions) {
+                final ContentValues contentValues = region.toContentValues();
+                database.insert(Players.TAG, null, contentValues);
+            }
+
+            database.setTransactionSuccessful();
+            database.endTransaction();
+            Database.stop();
+
+            mResponse.success(regions);
+        }
+
+
+        private void readThenMake() {
+            final SQLiteDatabase database = Database.start();
+            final Cursor cursor = database.query(Regions.TAG, null, null, null, null, null, null);
+
+            final ArrayList<Region> regions;
+
+            if (cursor.moveToFirst()) {
+                regions = new ArrayList<>();
+                final int idIndex = cursor.getColumnIndexOrThrow(Constants.ID);
+                final int nameIndex = cursor.getColumnIndexOrThrow(Constants.NAME);
+
+                do {
+                    final String id = cursor.getString(idIndex);
+                    final String name = cursor.getString(nameIndex);
+                    final Region region = new Region(id, name);
+                    regions.add(region);
+                } while (cursor.moveToNext());
+            } else {
+                regions = null;
+            }
+
+            cursor.close();
+            Database.stop();
+
+            if (regions == null || regions.isEmpty()) {
+                super.make();
+            } else {
+                mResponse.success(regions);
+            }
+        }
+
+
     }
 
 

5dc2290b1e9b657da6051777d78a3996da2e0e22
Charles Madere <charlesrmadere@gmail.com>
2015-04-18 14:49:59 -0700

    placeholder new fragment classes

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/ToolbarRegionsFragment.java
@@ -0,0 +1,19 @@
+package com.garpr.android.fragments;
+
+
+public class ToolbarRegionsFragment extends BaseFragment {
+
+
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+
+
+    @Override
+    protected String getFragmentName() {
+        return null;
+    }
+
+
+}

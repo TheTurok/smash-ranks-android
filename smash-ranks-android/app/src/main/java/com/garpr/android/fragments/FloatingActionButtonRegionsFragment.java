141aba71280943c58032785e4d433338a350422f
Charles Madere <charlesrmadere@gmail.com>
2015-04-19 12:24:19 -0700

    added barebones of new RegionsFragment containers

--- smash-ranks-android/app/src/main/java/com/garpr/android/fragments/FloatingActionButtonRegionsFragment.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/FloatingActionButtonRegionsFragment.java
@@ -1,18 +1,26 @@
 package com.garpr.android.fragments;
 
 
+import com.garpr.android.R;
+
+
 public class FloatingActionButtonRegionsFragment extends BaseFragment {
+
+
+    private static final String TAG = "FloatingActionButtonRegionsFragment";
+
+
 
 
     @Override
     protected int getContentView() {
-        return 0;
+        return R.layout.fragment_floating_action_button_regions;
     }
 
 
     @Override
     protected String getFragmentName() {
-        return null;
+        return TAG;
     }
 
 
--- smash-ranks-android/app/src/main/java/com/garpr/android/fragments/ToolbarRegionsFragment.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/ToolbarRegionsFragment.java
@@ -1,18 +1,26 @@
 package com.garpr.android.fragments;
 
 
+import com.garpr.android.R;
+
+
 public class ToolbarRegionsFragment extends BaseFragment {
+
+
+    private static final String TAG = "ToolbarRegionsFragment";
+
+
 
 
     @Override
     protected int getContentView() {
-        return 0;
+        return R.layout.fragment_toolbar_regions;
     }
 
 
     @Override
     protected String getFragmentName() {
-        return null;
+        return TAG;
     }
 
 

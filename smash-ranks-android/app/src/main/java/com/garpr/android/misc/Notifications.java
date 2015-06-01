5ea9127fb40b42a93e803c1552f1071f552f8e4e
Charles Madere <charlesrmadere@gmail.com>
2015-05-31 22:04:13 -0700

    migrated to NotificationManagerCompat

--- smash-ranks-android/app/src/main/java/com/garpr/android/misc/Notifications.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/misc/Notifications.java
@@ -1,12 +1,11 @@
 package com.garpr.android.misc;
 
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.garpr.android.App;
 import com.garpr.android.R;
@@ -28,14 +27,7 @@ public final class Notifications {
 
 
     public static void clear() {
-        final NotificationManager nm = getNotificationManager();
-        nm.cancelAll();
-    }
-
-
-    private static NotificationManager getNotificationManager() {
-        final Context context = App.getContext();
-        return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManagerCompat.from(App.getContext()).cancelAll();
     }
 
 
@@ -59,9 +51,7 @@ public final class Notifications {
                 PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(pendingIntent);
 
-        final NotificationManager nm = getNotificationManager();
-        final Notification notification = builder.build();
-        nm.notify(NOTIFICATION_ID, notification);
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build());
     }
 
 

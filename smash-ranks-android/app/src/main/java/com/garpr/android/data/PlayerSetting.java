1cfc8ff98df0d5f762c926e9e1c8e7a84dffacca
Charles Madere <charlesrmadere@gmail.com>
2015-06-06 01:33:37 -0700

    added PlayerSetting class

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data/PlayerSetting.java
@@ -0,0 +1,49 @@
+package com.garpr.android.data;
+
+
+import com.garpr.android.models.Player;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class PlayerSetting extends Setting<Player> {
+
+
+    private final JSONSetting mJSONSetting;
+
+
+
+
+    PlayerSetting(final String key) {
+        super(key);
+        mJSONSetting = new JSONSetting(key);
+    }
+
+
+    @Override
+    public Player get() {
+        final JSONObject json = mJSONSetting.get();
+        final Player player;
+
+        if (json == null) {
+            player = null;
+        } else {
+            try {
+                player = new Player(json);
+            } catch (final JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return player;
+    }
+
+
+    @Override
+    public void set(final Player newValue) {
+        mJSONSetting.set(newValue.toJSON());
+    }
+
+
+}

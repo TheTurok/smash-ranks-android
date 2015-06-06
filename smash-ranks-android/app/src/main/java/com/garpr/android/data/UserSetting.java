5108abf158c55807302f43ed637ff5a9d93a4fc3
Charles Madere <charlesrmadere@gmail.com>
2015-06-05 22:22:20 -0700

    added placeholder UserSetting class

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data/UserSetting.java
@@ -0,0 +1,27 @@
+package com.garpr.android.data;
+
+
+public class UserSetting extends Setting<User> {
+
+
+    // TODO
+
+
+    UserSetting(final String key) {
+        super(key);
+    }
+
+
+    @Override
+    public User get() {
+        return null;
+    }
+
+
+    @Override
+    public void set(final User newValue) {
+
+    }
+
+
+}

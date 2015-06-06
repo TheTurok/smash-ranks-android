629afcb74caf7b25977d1fe73a7215a79a81aaa2
Charles Madere <charlesrmadere@gmail.com>
2015-06-06 14:08:08 -0700

    imports optimized

--- smash-ranks-android/app/src/main/java/com/garpr/android/fragments/PlayersFragment.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/PlayersFragment.java
@@ -17,7 +17,6 @@ import com.garpr.android.App;
 import com.garpr.android.R;
 import com.garpr.android.calls.Players;
 import com.garpr.android.calls.ResponseOnUi;
-import com.garpr.android.data.Settings;
 import com.garpr.android.misc.Console;
 import com.garpr.android.misc.ListUtils;
 import com.garpr.android.misc.ListUtils.AlphabeticalSectionCreator;

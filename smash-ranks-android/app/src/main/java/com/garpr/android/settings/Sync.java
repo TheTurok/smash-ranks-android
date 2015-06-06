3e85a0db1723d3044f3ca0b152c45385fb609f46
Charles Madere <charlesrmadere@gmail.com>
2015-06-06 14:59:43 -0700

    Added sync settings

--- smash-ranks-android/app/src/main/java/com/garpr/android/settings/Sync.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/settings/Sync.java
@@ -6,5 +6,18 @@ public final class Sync {
 
     private static final String CNAME = "com.garpr.android.settings.Sync";
 
+    public static final BooleanSetting ChargingNecessary;
+    public static final BooleanSetting Enabled;
+    public static final BooleanSetting WifiNecessary;
+
+
+
+
+    static {
+        ChargingNecessary = new BooleanSetting(CNAME + ".CHARGING_NECESSARY", false);
+        Enabled = new BooleanSetting(CNAME + ".ENABLED", true);
+        WifiNecessary = new BooleanSetting(CNAME + ".WIFI_NECESSARY", true);
+    }
+
 
 }

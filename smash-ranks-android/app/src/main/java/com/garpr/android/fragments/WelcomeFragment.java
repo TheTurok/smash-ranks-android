3fbdda2a7b99e44fcd48b715d04535b9fe7390c9
Charles Madere <charlesrmadere@gmail.com>
2015-06-12 00:25:26 -0700

    added initial animation setup, this might change

--- smash-ranks-android/app/src/main/java/com/garpr/android/fragments/WelcomeFragment.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/WelcomeFragment.java
@@ -1,11 +1,14 @@
 package com.garpr.android.fragments;
 
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.garpr.android.R;
@@ -16,8 +19,11 @@ public class WelcomeFragment extends BaseFragment {
 
     private static final String TAG = "WelcomeFragment";
 
+    private AccelerateDecelerateInterpolator mAnimationInterpolator;
     private ImageButton mNext;
+    private ImageView mOrb;
     private Listener mListener;
+    private TextView mGarPr;
     private TextView mWelcomeText;
 
 
@@ -28,9 +34,30 @@ public class WelcomeFragment extends BaseFragment {
     }
 
 
+    private void animateView(final View view, final long duration) {
+        final ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                view.setAlpha((Float) animation.getAnimatedValue());
+            }
+        });
+
+        if (mAnimationInterpolator == null) {
+            mAnimationInterpolator = new AccelerateDecelerateInterpolator();
+        }
+
+        animator.setInterpolator(mAnimationInterpolator);
+        animator.setDuration(duration);
+        animator.start();
+    }
+
+
     private void findViews() {
         final View view = getView();
+        mGarPr = (TextView) view.findViewById(R.id.fragment_welcome_gar_pr);
         mNext = (ImageButton) view.findViewById(R.id.fragment_welcome_next);
+        mOrb = (ImageView) view.findViewById(R.id.fragment_welcome_orb);
         mWelcomeText = (TextView) view.findViewById(R.id.fragment_welcome_text);
     }
 
@@ -71,6 +98,11 @@ public class WelcomeFragment extends BaseFragment {
         });
 
         mWelcomeText.setText(Html.fromHtml(getString(R.string.gar_pr_welcome_text)));
+
+        animateView(mOrb, 750L);
+        animateView(mGarPr, 1000L);
+        animateView(mWelcomeText, 2000L);
+        animateView(mNext, 3000L);
     }
 
 
